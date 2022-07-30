29f7ad89158048518f7467d039a1026f267136ef
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-07-30 00:02:02 -0500

    Update events.rs
    camera moves with bailey

--- src/char/events.rs
+++ src/char/events.rs
@@ -1,8 +1,8 @@
 //! Module for events and interactions between chars
-use bevy::{prelude::*,core::FixedTimestep};
+use bevy::{prelude::*,core::FixedTimestep,render::camera::Camera2d};
 
 //Constants
-const TIME_STEP: f32 = 1.0 / 300.0;
+pub const TIME_STEP: f32 = 1.0 / 300.0;
 pub const BAILEY_SPEED: f32 = 1000.0;
 
 pub struct Events;
@@ -11,6 +11,7 @@ impl Plugin for Events {
         app.add_system_set(SystemSet::new()
             .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
             .with_system(swim)
+            .with_system(cam)
             );
     }
 }
@@ -47,4 +48,17 @@ fn swim(buttons:Res<Input<MouseButton>>,
     let bailey_posx=bailey_trans.translation.x+dirx*BAILEY_SPEED*TIME_STEP;
     bailey_trans.translation.y=bailey_posy;
     bailey_trans.translation.x=bailey_posx;
+}
+
+pub fn cam(buttons:Res<Input<MouseButton>>,
+    mut query:Query<&mut Transform, With<Camera2d>>) { //would love to figure out how to combine moving things at the same time so i wouldnt have to make multiple functions like this- just throwing them in the same function seems to crash bevy
+
+    let mut cam_trans=query.single_mut();
+    let mut dirx=0.0;
+
+    if buttons.any_just_pressed([MouseButton::Left,MouseButton::Right]) { //checks to see if either left or right mouse buttons were pressed
+        dirx+=7.5;
+    }
+    let cam_posx=cam_trans.translation.x+dirx*BAILEY_SPEED*TIME_STEP;
+    cam_trans.translation.x=cam_posx;
 }
